e2cef887e6a051e3fa08e75e6aa3e443b872fa65
Suhut <suhut.wadiyo@gmail.com>
2020-01-11 10:54:50 +0700

    first commit

---
+++ SapHanaEf/HANAConsole.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sap.Data.Hana;
+
+namespace SapHanaEf
+{
+    public class HANAConsole
+    {
+        static void Main(string[] args)
+        {
+            //testInsertEf(); 
+            testInsertEfOnTheFlyConn();
+        }
+
+        static void testInsertEfOnTheFlyConn()
+        {
+            using (var context = IDU_HANA_APP.Create())
+            {
+                var tm_Master01 = new Tm_Master01();
+                tm_Master01.Description = "ini di insert dari EF on the flay conn";
+                context.Tm_Master01.Add(tm_Master01);
+                context.SaveChanges();
+            } 
+        }
+
+        //static void testInsertEf()
+        //{
+        //    using (var context = new Entities1())
+        //    {
+        //        var tm_Master01 = new Tm_Master01();
+        //        tm_Master01.Description = "ini di insert dari EF";
+        //        context.Tm_Master01.Add(tm_Master01);
+        //        context.SaveChanges();
+        //    }
+        //}
+
+        static void testKoneksiBiasa()
+        {
+            HanaConnection conn = new HanaConnection("Server=$userHost$:$userPort$;UserName=$userName$;Password=$userPassword$");
+            conn.Open();
+            HanaCommand cmd = new HanaCommand("select 'Hello, World' from DUMMY", conn); // Exception
+            try
+            {
+                HanaDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+                Console.WriteLine(reader.GetString(0));
+            }
+            catch (Exception e)
+            {
+                String errorMsg = e.ToString();
+                Console.WriteLine(errorMsg);
+            }
+        }
+    }
+}
---
+++ SapHanaEf/IDU_HANA_APP.cs
@@ -0,0 +1,50 @@
+﻿using Sap.Data.Hana;
+using System;
+using System.Collections.Concurrent;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace SapHanaEf
+
+{
+    //implementasi
+    //https://stackoverflow.com/questions/11602409/execute-as-when-using-a-dbcontext-with-linq 
+
+    public class IDU_HANA_APP
+    {
+        private static ConcurrentDictionary<Tuple<string, string>, DbCompiledModel> modelCache = new ConcurrentDictionary<Tuple<string, string>, DbCompiledModel>();
+
+        public static Entities1 Create()
+        {
+            string schemaName = "SUHUT_DATAACCESS";
+            DbCompiledModel compiledModel; 
+
+            string connString = ConfigurationManager.ConnectionStrings["Model1"].ToString(); 
+
+            using (var connection = new HanaConnection(connString))
+            {
+                //connection.Open();
+                compiledModel = modelCache.GetOrAdd(
+             Tuple.Create(connection.ConnectionString, schemaName),
+             t =>
+             {
+                 var builder = new DbModelBuilder();
+                 //builder.Conventions.Remove<PluralizingTableNameConvention>();
+                 //builder.Conventions.Remove<ForeignKeyIndexConvention>();
+                 builder = IduEfConfigurations.SetConfigurations(builder, schemaName);
+
+                 var model = builder.Build(connection);
+                 return model.Compile();
+             });
+                return new Entities1(connString, compiledModel);
+            }
+
+        }
+
+
+
+    }
+
+
+}
